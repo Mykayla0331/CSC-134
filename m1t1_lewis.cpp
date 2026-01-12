b62ab71bc3530ee1e93b362f316728fd9367629e
Mykayla0331 <lewism1735@student.faytechcc.edu>
2026-01-12 16:35:40 +0000

    Finished m1t1

---
+++ m1t1_lewis.cpp
@@ -0,0 +1,13 @@
+// CSC 124
+// M1T1 (Module 1, Tutorial 1)
+// Hello, World!
+
+// magic words
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl; // E N D L
+    cout << endl; // another new line
+    return 0; // no errors
+}
