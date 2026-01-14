fc5b31ed662bd950c2798167f48cb2b02c0696b1
Mykayla0331 <lewism1735@student.faytechcc.edu>
2026-01-14 16:01:50 +0000

    Added more cout statements

--- m1t1_lewis.cpp
+++ m1t1_lewis.cpp
@@ -8,6 +8,13 @@ using namespace std;
 
 int main() {
     cout << "Hello, World!" << endl; // E N D L
+    cout << "WHAT'SSSS GOOOOODDDDD!!!!" << endl;
     cout << endl; // another new line
+
+    /*
+    next we will:
+    - make a variable (a noun) to store the user's name*/
+    - ask them by the name
+    - greet them by name 
     return 0; // no errors
 }
