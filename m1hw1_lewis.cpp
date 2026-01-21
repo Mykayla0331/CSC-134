7c28b460a49c02c64ad0aaaaa05730319659d636
Mykayla0331 <lewism1735@student.faytechcc.edu>
2026-01-21 16:49:22 +0000

    finished m1hw1

--- m1hw1_lewis.cpp
+++ m1hw1_lewis.cpp
@@ -2,10 +2,19 @@
 // m1hw1 - movie talk
 // mykayla
 // 1-21-2026
+#include <iostream>
+using namespace std;
 
-int main(){
-string item_name "A dog's purpose"; // movie title
+int main() {
 
-cout << "www" << item_name << "sww" << endl;
-return 0;    
+    string movie_name = "Top gun"; // movie title
+    int movie_year = 1986;
+    double movie_gross = 1.49;
+    cout << "The movie, " << movie_name << " came out in " << movie_year << " and grossed $" << movie_gross << " billion worldwide" << endl;
+    cout << "A quote from the movie," << endl;
+    cout << "Maverick: I feel the need..." << endl;
+    cout << "Maverick, Goose: ...the need for speed!" << endl;
+
+
+    return 0;    
 }
