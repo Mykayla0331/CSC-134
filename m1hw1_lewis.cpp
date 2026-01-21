157254948f8db41ee68aa1a683ab79f2ec847d98
Mykayla0331 <lewism1735@student.faytechcc.edu>
2026-01-21 16:46:45 +0000

    finished m1hw1

---
+++ m1hw1_lewis.cpp
@@ -0,0 +1,11 @@
+// csc 134
+// m1hw1 - movie talk
+// mykayla
+// 1-21-2026
+
+int main(){
+string item_name "A dog's purpose"; // movie title
+
+cout << "www" << item_name << "sww" << endl;
+return 0;    
+}
