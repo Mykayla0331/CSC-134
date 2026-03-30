bda12171f5cfb5c3d687034ac67c57f180dac3c4
Mykayla0331 <lewism1735@student.faytechcc.edu>
2026-03-30 15:24:51 +0000

    completed m4hw1

--- M4HW1.cpp
+++ M4HW1.cpp
@@ -2,3 +2,21 @@
 // silver
 // 3-30-26
 // mykayla lewis
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // declare variables
+    int firstnum, secondnum, answer;
+    answer = firstnum * secondnum;
+
+    cout << "Insert the first number, 1-12. ";
+    cin >> firstnum;
+
+    for (int i=1; i<=12; i++) {
+        answer = firstnum * i;
+        cout << firstnum << " times " << i << " is " << answer << endl;
+    }
+}
