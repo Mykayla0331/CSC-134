41905842311da5984a3032126ee67e75cac3fd03
Mykayla0331 <lewism1735@student.faytechcc.edu>
2026-04-27 14:41:54 +0000

    created m7t1

---
+++ M7T1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+// CSC 134
+// M7T1 - Restaurant Rating
+// Mykayla lewis
+// 4/27/26
+// Use Restaurant class to store user ratings
+
+// Next time we'll put the class in a separate file
+class Restaurant {
+  private:
+    string name;    // the name
+    double rating;  // 0 to 5 stars
+
+  public:
+	// constructor 
+	Restaurant(string n, double r) {
+		name = n;
+		rating = r;
+	}
+	// getters and setters
+    void setName(string n) {
+        name = n; 
+    }
+    void setRating(double r) {
+        rating = r;
+    }
+    string getName() const {
+        return name;
+    }
+    double getRating() const {
+        return rating;
+    }
+  
+};
+
+int main() {
+    cout << "M7T1 - Restaurant Reviews" << endl;
+
+    return 0;
+
+}
